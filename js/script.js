37cfbec2fb0fff518e4d6cd9689f4952282d518b
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2021-09-22 20:41:24 +0200

    Refactored code

--- js/script.js
+++ js/script.js
@@ -1,29 +1,42 @@
 const keyElem = document.querySelector('#key');
 const keyCodeElem = document.querySelector('#keyCode');
 
-const loggPressedKey = (e) => {
-    console.log(e);    
-    keyElem.classList.remove('js-keyPressAnim');
-    keyCodeElem.classList.remove('js-keyPressAnim');
+const loggPressedKey = (e) => { 
+    removeAnimationClass();
 
     let key = e.key;
     let code = e.keyCode;
 
-    if (e.keyCode === 32) {
+    if (code === 32) {
         key = 'Space';
     }
 
-    keyElem.innerText = `${key}`;
-    keyCodeElem.innerText = `${code}`;
+    populatedElems(key, code);
     
     setTimeout(() => {        
-        keyElem.classList.add('js-keyPressAnim');
-        keyCodeElem.classList.add('js-keyPressAnim');
+        addAnimationClass();
     }, 20);
 }
+
+const removeAnimationClass = () => {
+    keyElem.classList.remove('js-keyPressAnim');
+    keyCodeElem.classList.remove('js-keyPressAnim');
+}
+const addAnimationClass = () => {
+    keyElem.classList.add('js-keyPressAnim');
+    keyCodeElem.classList.add('js-keyPressAnim');
+}
+
+const populatedElems = (key, code) => {
+    keyElem.innerText = key;
+    keyCodeElem.innerText = code;
+}
+
+removeAnimationClass();
 
 document.addEventListener('keydown', e => {
     e.preventDefault();;
 });
 
 document.addEventListener('keyup', loggPressedKey);
+
